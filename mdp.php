9d85d1fbb6524885fac56c0da84a90ccf61de7b3
falconika <khenchouchakarim@gmail.com>
2017-02-02 11:28:55 +0100

    debut et fin du tp mdp

--- mdp.php
+++ mdp.php
@@ -1,1 +1,8 @@
-<?php include'menu.php' ;?>
+<?php include'menu.php';
+echo "Entrer mot de passe";
+;?>
+
+<form class="" action="le_secret.php" method="post">
+  <label for="mdp">Entrer votre mot de passe de 4 caractères au  maximum</label>
+  <input type="password" name="mdp" maxlength="4">
+  <input type="submit"  value="valider">
